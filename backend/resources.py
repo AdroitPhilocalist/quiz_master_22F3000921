a6520f77c50cb3cb4fd27a5e34b4561cb83e1d2e
Piyush Saha <sahapiyush5@gmail.com>
2025-03-13 02:41:44 +0530

    add CSV import functionality to resources

--- backend/resources.py
+++ backend/resources.py
@@ -1,3 +1,5 @@
+import csv
+from io import StringIO
 from flask import jsonify, request
 from flask_restful import Api, Resource, fields, marshal_with
 from flask_security import auth_required, current_user, roles_required
